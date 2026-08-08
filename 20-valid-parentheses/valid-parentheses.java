b33be8bd685c60d0cedd275ce55ece449520d46a
slaykee404 <keerthanabaijuchandran@gmail.com>
2026-08-08 11:17:22 +0530

    Time: 3 ms (86.01%) | Memory: 43.3 MB (41.51%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[' ){
+                st.push(ch);
+            }
+            else{
+                if (st.isEmpty()){
+                    return false;
+                }
+                else{
+                    char top=st.peek();
+                    if((ch==')' && top!='(' )||(ch=='}' && top!='{' )||(ch==']' && top!='[' ))
+                    {
+                        return false;
+                    }
+                    st.pop();
+                    }
+                }
+            }
+            return st.isEmpty();
+
+        }
+    }
+
+        
+    
